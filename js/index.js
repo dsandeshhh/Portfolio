615f41edd0513a606f794e2f64850dceb657bba7
sandeshh dhakal <dsandeshhh@gmail.com>
2023-10-19 14:41:54 +0545

    firstCommit

---
+++ js/index.js
@@ -0,0 +1,24 @@
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value,
+    };
+
+    const serviceID = "service_wbpz2st";
+    const templateID = "template_bm7ftm5";
+
+    emailjs.send(serviceID, templateID, params)
+        .then(res => {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("subject").value = "";
+            document.getElementById("message").value = "";
+            console.log(res);
+            alert("Gracias, your message has been sent successfully!!")
+
+        })
+        .catch(err => console.log(err));
+
+}
